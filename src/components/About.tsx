a21a0a345dedeb7d980ba455170f06eef427e8af
Rithvik Pallamreddy <152109655+Rithvik8001@users.noreply.github.com>
2024-10-28 11:44:42 -0400

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,43 +12,28 @@ export default function page() {
       <BlurFade delay={0.35} inView>
         <div className="max-w-4xl mx-auto space-y-4 mt-14">
           <p className="text-gray-700 text-xl">
-            Hi,This is Rithvik.I am a Computer Science Graduate and a self
-            taught Web Developer. I Specialze in creating web applications,
-            leveraging my technical expertise to solve complex problems. I would
-            love to have an oppurtunity where i can showcase and enchance my
-            skills.
+           Hi, this is Rithvik. I am a Computer Science graduate and a self-taught web developer specializing in creating web applications. I leverage my technical expertise to solve complex problems and would love the opportunity to showcase and enhance my skills.
           </p>
         </div>
       </BlurFade>
       <BlurFade delay={0.45} inView>
         <div className="max-w-4xl mx-auto space-y-4 mt-14">
           <p className="text-gray-700 text-xl">
-            My journey began with the basics of HTML , CSS , and JavaScript ,
-            which laid a strong foundation for my frontend development skills.
-            Over time, I have expanded my expertise to include modern frameworks
-            and libraries such as React , also currently learning NextJs and
-            Tailwind CSS .{" "}
+           My journey began with the basics of HTML, CSS, and JavaScript, which laid a strong foundation for my frontend development skills. Over time, I have expanded my expertise to include modern frameworks and libraries such as React. I am also currently learning Next.js and Tailwind CSS.{" "}
           </p>
         </div>
       </BlurFade>
       <BlurFade delay={0.55} inView>
         <div className="max-w-4xl mx-auto space-y-4 mt-14">
           <p className="text-gray-700 text-xl">
-            While I am not coding, I Love to watch Football⚽️. You can call me
-            a football fanatic, I started watching this beautiful sport when I
-            was 16 years old, and my favorite club is the biggest club in
-            England, Manchester United!
+           When I'm not coding, I love to watch football ⚽️. You could call me a football fanatic—I started watching this beautiful sport when I was 16 years old, and my favorite club is the biggest in England, Manchester United!
           </p>
         </div>
       </BlurFade>
       <BlurFade delay={0.65} inView>
         <div className="max-w-4xl mx-auto space-y-4 mt-14">
           <p className="text-gray-700 text-xl">
-            My goal is to leverage my skills and knowledge to create impactful
-            web applications that solve real-world problems. I am excited about
-            the opportunities in frontend development and look forward to
-            collaborating with like-minded professionals to build innovative
-            solutions.
+           My goal is to leverage my skills and knowledge to create impactful web applications that solve real-world problems. I am excited about opportunities in frontend development and look forward to collaborating with like-minded professionals to build innovative solutions.
           </p>
         </div>
       </BlurFade>
